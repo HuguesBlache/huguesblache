2c4eb2323ef539fff8708fd6d4eeeaea36d66c79
HuguesBlache <hugues.blache@polymtl.ca>
2025-04-29 10:05:23 +1000

    fix error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-
 import Face from "@/components/landing/Face";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,12 +8,12 @@ export default function RootLayout({
   return (
     <div className="flex size-full">
       {/* Header sur la gauche (1/3 de la page) */}
-      <div className="w-1/3 h-full border-r">
+      <div className="h-full w-1/3 border-r">
         <Face />
       </div>
       
       {/* Contenu principal sur la droite (2/3 de la page) */}
-      <div className="w-2/3 h-full overflow-auto flex flex-col">
+      <div className="flex h-full w-2/3 flex-col overflow-auto">
         <div className="flex-1 overflow-hidden">
           {children}
         </div>
